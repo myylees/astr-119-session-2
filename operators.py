89fa6ea72f392bb9c8f49ed9f860faa4a1df767d
myylees <43651697+myylees@users.noreply.github.com>
2018-10-02 11:26:24 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 9	#make x equal 9
+y = 3	#make y equal 3
+
+#Arithmetic operators
+print(x+y)	#addition
+print(x-y)	#subtraction
+print(x*y)	#multiplication
+print(x/y)	#division
+print(x%y)	#modulus
+print(x**y)	#exponentiation
+x = 9.191823
+print(x//y)	#Floor division
+
+# Assignment operators
+x = 9		# set x = 9
+x += 3		# x = x + 3
+print(x)
+x = 9
+x -= 3		# x = x - 3
+print(x)
+x*= 3		# x = x * 3
+print(x)
+x /= 3		# x  = x / 3
+print(x)
+x **= 3		# x = x ** 3
+print(x)
+
+# Comparison operators
+x = 9
+y = 3
+print(x==y)		#True if x equals y, False otherwise
+print(x!=y)		#True if x does not equal y, False otherwise
+print(x>y)		#True if x is greater than y. False otherwise
+print(x<y)		#True if x is less than y, False otherwise
+print(x>=y)		#True if x is greater than or equal to y, False otherwise
+print(x<=y)		#True if x is less than or equal to y, False otherwise
